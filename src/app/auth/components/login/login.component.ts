f0647cd88e3be77ff5416265983f4f1b2196be23
Uriel <urielenrique99@gmail.com>
2021-04-29 09:08:04 -0500

    Se agregaron validaciones al login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidationsService } from '../../services/validations.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login-component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  loginFormulario: FormGroup = this._fb.group({
+    email : ['', [Validators.required, Validators.pattern(this._validationService.emailRegExp)]],
+    password : ['', [Validators.required, Validators.pattern(this._validationService.passRegExp)]]
+  })
+
+  constructor(private _fb: FormBuilder,
+              private _validationService: ValidationsService) { }
+
+
+  campoValido(campo : string){
+    return this.loginFormulario.get(campo)!.invalid && this.loginFormulario.get(campo)!.touched;
+  }
+
+  mensajeError(campo : string): string{
+    let errores = this.loginFormulario.get(campo)?.errors;
+    return this._validationService.obtenerMensaje(errores);
+  }
+
+  ingresar(): void{
+    if(this.loginFormulario.invalid){
+      this.loginFormulario.markAllAsTouched();
+      Swal.fire({
+        title : 'Error de validación',
+        text : 'Al parecer los datos que ingresaste no son como los que esperabamos, por favor, intenta nuevamente',
+        icon : 'error',
+        confirmButtonText : 'Aceptar'
+      });
+      return;
+    }
+
+    console.log(this.loginFormulario.value);
   }
 
 }
--- src/app/auth/components/registro/registro.component.ts
+++ src/app/auth/components/registro/registro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidationsService } from '../../services/validations.service';
 import Swal from 'sweetalert2'
@@ -8,49 +8,46 @@ import Swal from 'sweetalert2'
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent implements OnInit {
+export class RegistroComponent {
 
   formularioRegistro: FormGroup = this._fb.group({
-    nombre : ['', [Validators.required, Validators.pattern(this._validationsService.nombreRegExp)]],
-    apPat : ['', [Validators.required, Validators.pattern(this._validationsService.nombreRegExp)]],
-    email : ['', [Validators.required, Validators.pattern(this._validationsService.emailRegExp)]],
-    password : ['', [Validators.required, Validators.pattern(this._validationsService.passRegExp)]],
-    passwordC : ['', [Validators.required]]
+    nombre: ['', [Validators.required, Validators.pattern(this._validationsService.nombreRegExp)]],
+    apPat: ['', [Validators.required, Validators.pattern(this._validationsService.nombreRegExp)]],
+    email: ['', [Validators.required, Validators.pattern(this._validationsService.emailRegExp)]],
+    password: ['', [Validators.required, Validators.pattern(this._validationsService.passRegExp)]],
+    passwordC: ['', [Validators.required]]
   }, {
-    validators : [this._validationsService.contraseniaIgual('password', 'passwordC')]
+    validators: [this._validationsService.contraseniaIgual('password', 'passwordC')]
   });
 
 
   constructor(private _fb: FormBuilder,
-              private _validationsService: ValidationsService) { }
+    private _validationsService: ValidationsService) { }
 
-  ngOnInit(): void {
-  }
-
-  campoValido(campo : string): boolean{
+  campoValido(campo: string): boolean {
     return this.formularioRegistro.get(campo)!.invalid && this.formularioRegistro.get(campo)!.touched;
   }
 
-  mensajeError(campo : string): string{
+  mensajeError(campo: string): string {
     let errores = this.formularioRegistro.get(campo)?.errors;
     return this._validationsService.obtenerMensaje(errores);
   }
 
-  guardar(): void{
+  guardar(): void {
 
-    if(this.formularioRegistro.valid){
-      console.log(this.formularioRegistro.value);
+    if (this.formularioRegistro.invalid) {
+      this.formularioRegistro.markAllAsTouched();
+      Swal.fire({
+        title: 'Errores de validación',
+        text: 'Al parecer los datos que ingresaste no son como los que esperabamos, por favor, intenta nuevamente',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      });
       return;
     }
-    this.formularioRegistro.markAllAsTouched();
 
-    Swal.fire({
-      title : 'Errores de validación',
-      text : 'Al parecer los datos que ingresaste no son como los que esperabamos, por favor, intenta nuevamente',
-      icon : 'error',
-      confirmButtonText : 'Aceptar'
-    });
-    
+    console.log(this.formularioRegistro.value);
+
   }
 
 }
